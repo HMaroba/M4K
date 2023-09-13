362005effc3905cc679e513035aba296940084f8
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-13 12:47:49 +0200

    Added Border

--- lib/screens/customerDashboard/settings.dart
+++ lib/screens/customerDashboard/settings.dart
@@ -20,23 +20,79 @@ class SettingPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Settings"),
         ),
-        body: ListView(
-          children: const [
-            ListTile(
-              leading: Icon(Icons.facebook),
-              title: Text("Facebook"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ListView(
+              children: const [
+                ListTile(
+                  leading: Icon(
+                    Icons.info,
+                    size: 30,
+                    color: Colors.pink,
+                  ),
+                  title: Text(
+                    "About",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30,
+                    color: Colors.pink,
+                  ),
+                  title: Text(
+                    "Contact Us",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.wallet,
+                    size: 30,
+                    color: Colors.pink,
+                  ),
+                  title: Text(
+                    "Payments",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.wallet_giftcard_outlined,
+                    size: 30,
+                    color: Colors.pink,
+                  ),
+                  title: Text(
+                    "My Rewards",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    size: 30,
+                    color: Colors.pink,
+                  ),
+                  title: Text(
+                    "Logout",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                )
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.facebook),
-              title: Text("Facebook"),
+            const SizedBox(
+              height: 34,
             ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Settings"),
-            ),
-            ListTile(
-              leading: Icon(Icons.call),
-              title: Text("Call"),
+            const Text(
+              'Feedback',
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 27),
             )
           ],
         ),
