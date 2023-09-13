8e1aab4f099ae818ec40ee00da572e160d24b281
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-13 10:40:17 +0200

    Created profile page

--- lib/screens/customerDashboard/bookings.dart
+++ lib/screens/customerDashboard/bookings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:m4k/screens/customerDashboard/user_bookings.dart';
 
 class CustomerBookings extends StatefulWidget {
   const CustomerBookings({super.key});
@@ -15,7 +16,59 @@ class _CustomerBookingsState extends State<CustomerBookings> {
         title: const Text("My Bookings"),
         centerTitle: true,
       ),
-      body: const Center(child: Text("Booking List")),
+      body: SingleChildScrollView(
+        child: ListView.builder(
+          shrinkWrap:
+              true, // Ensure the ListView doesn't take more space than needed
+          itemCount: bookingList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = bookingList[index];
+            return Card(
+              elevation: 2, // Add elevation for a card-like appearance
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: ListTile(
+                title: Text(
+                  item['name'],
+                  style: const TextStyle(fontSize: 19),
+                ),
+                // subtitle: Column(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   children: [
+                //     Text(
+                //       'Price: ${item['price']}',
+                //       style: const TextStyle(fontSize: 16),
+                //     ),
+                //     Text(
+                //       'DateTime: ${item['pickupdateTime']}',
+                //       style: const TextStyle(fontSize: 16),
+                //     ),
+                //   ],
+                // ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Price: ${item['price']}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'Pickup Date/Time: ${item['pickupdateTime']}',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                trailing: Text(
+                  '${item['status']}',
+                  style: const TextStyle(fontSize: 20, color: Colors.pink),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/book');
