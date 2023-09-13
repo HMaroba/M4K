f5f4bf0c27d6f27c61534c8c17d15c9d3dc42270
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-13 10:09:25 +0200

    Created profile page

--- lib/screens/customerDashboard/home.dart
+++ lib/screens/customerDashboard/home.dart
@@ -15,6 +15,36 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Container(
+              width: double.infinity,
+              height: 150,
+              decoration: const ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(64),
+                    bottomRight: Radius.circular(64),
+                  ),
+                ),
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFFFD0E42),
+                    Color(0xFFC30F31),
+                  ],
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.only(top: 46.0, left: 20.0),
+                child: Text(
+                  'Discover',
+                  style: TextStyle(
+                    fontFamily: 'Nunito',
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white,
+                    fontSize: 36,
+                  ),
+                ),
+              ),
+            ),
             const SizedBox(
               height: 100,
             ),
---
+++ lib/screens/customerDashboard/price_list.dart
@@ -0,0 +1,6 @@
+List<Map<String, dynamic>> priceList = [
+  {'name': 'Small Bucket', 'price': 'M50.00'},
+  {'name': 'Medium Bucket', 'price': 'M50.00'},
+  {'name': 'Large Bucket', 'price': 'M50.00'},
+  {'name': 'Sneakers', 'price': 'M20.00'},
+];
