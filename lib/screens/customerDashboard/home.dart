5e9d22368afb00f298135cffb87e8bc7a075814e
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-13 05:07:31 +0200

    Created profile page

--- lib/screens/customerDashboard/home.dart
+++ lib/screens/customerDashboard/home.dart
@@ -10,11 +10,63 @@ class HomePage extends StatelessWidget {
         title: const Text("Home Page"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Image.asset(
-          'assets/images/services.png',
-          height: 190,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            'assets/images/services.png',
+            height: 190,
+          ),
+          // const SizedBox(
+          //   height: 30,
+          // ),
+          // Image.asset(
+          //   'assets/images/bookNow.png',
+          //   height: 190,
+          //   width: double.infinity,
+          // ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Our Prices',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                'Small Backet : M50',
+                style: TextStyle(fontSize: 19),
+              ),
+              Text(
+                'Medium Backet : M70',
+                style: TextStyle(fontSize: 19),
+              ),
+              // Text('Large Backet : M100'),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                'Large Backet : M100',
+                style: TextStyle(fontSize: 19),
+              ),
+              Text(
+                'Sneakers : M20',
+                style: TextStyle(fontSize: 19),
+              ),
+              // Text('Large Backet : M100'),
+            ],
+          )
+        ],
       ),
     );
   }
