413415bb4db7dfd8589255962e8e2c670a50f7ef
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-15 16:00:08 +0200

    Added multiple users

--- lib/screens/splash/firstScreen.dart
+++ lib/screens/splash/firstScreen.dart
@@ -15,7 +15,7 @@ class _FirstSreenSplashState extends State<FirstSreenSplash> {
   }
 
   Future<void> _navigateToSecondScreen() async {
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 3));
     Navigator.pushReplacementNamed(context, '/splash');
   }
 
@@ -33,16 +33,6 @@ class _FirstSreenSplashState extends State<FirstSreenSplash> {
                 'assets/images/logo.jpg',
                 height: 200,
               ),
-            ),
-            const SizedBox(
-              height: 35.0,
-            ),
-            const Text(
-              "M4K LAUNDRY",
-              style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
             ),
           ],
         )),
