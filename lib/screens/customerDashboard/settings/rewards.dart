0b354e82b1d05df651a47974393a2dc856c1ccaf
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-16 08:57:37 +0200

    Developed new settings pageAA

--- lib/screens/customerDashboard/settings/rewards.dart
+++ lib/screens/customerDashboard/settings/rewards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:m4k/screens/customerDashboard/settings/rewards_list.dart';
 
 class RewardsScreen extends StatefulWidget {
   const RewardsScreen({super.key});
@@ -49,54 +50,47 @@ class _RewardsScreenState extends State<RewardsScreen> {
             const Padding(
               padding: EdgeInsets.only(
                 left: 16,
+                top: 30,
               ),
               child: Text(
-                'Coming Soon stay tuned',
+                'Unlock Amazing Rewards Shortly',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
               ),
             ),
-
-            // CarouselSlider.builder(
-            //   options: CarouselOptions(
-            //     height: 100.0,
-            //     enableInfiniteScroll: true,
-            //     viewportFraction: 0.9,
-            //     autoPlay: true,
-            //     enlargeCenterPage: true,
-            //     autoPlayInterval: const Duration(seconds: 4),
-            //     autoPlayAnimationDuration: const Duration(milliseconds: 800),
-            //     autoPlayCurve: Curves.fastOutSlowIn,
-            //     pauseAutoPlayOnTouch: true,
-            //     aspectRatio: 2.0,
-            //     onPageChanged: (index, reason) {
-            //       // Handle indicator change if needed
-            //     },
-            //     scrollDirection: Axis.horizontal,
-            //   ),
-            //   itemCount: catName.length,
-            //   itemBuilder: (BuildContext context, int index, int realIndex) {
-            //     final item = catName[index];
-            //     return Card(
-            //       elevation: 2,
-            //       margin:
-            //           const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            //       child: Column(
-            //         crossAxisAlignment: CrossAxisAlignment.start,
-            //         children: [
-            //           Padding(
-            //             padding:
-            //                 const EdgeInsets.only(left: 8, top: 16, bottom: 5),
-            //             child: Text(
-            //               item,
-            //               style: const TextStyle(
-            //                   fontSize: 20, fontWeight: FontWeight.w400),
-            //             ),
-            //           )
-            //         ],
-            //       ),
-            //     );
-            //   },
-            // ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: rewards.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final promotion = rewards[index];
+                  return Card(
+                    margin: const EdgeInsets.all(16),
+                    child: ListTile(
+                      // title: Text(promotion['title']!),
+                      // subtitle: Text(promotion['description']!),
+                      subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              promotion['title']!,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(promotion['description']!)
+                          ]),
+                      onTap: () {
+                        // Handle tapping on a promotion if needed
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
