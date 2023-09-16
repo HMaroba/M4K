bd04907f59729da1a86b3cbbac9ea07003eb6959
Coffee58511867 <marobahlalele910@gmail.com>
2023-09-16 08:49:29 +0200

    Developed new settings pageAA

---
+++ lib/screens/customerDashboard/settings/rewards_list.dart
@@ -0,0 +1,26 @@
+List<Map<String, String>> rewards = [
+  {
+    'title': '10% Discount on Next Booking',
+    'description': 'Get a 10% discount on your next booking with us.',
+  },
+  {
+    'title': 'Free Pickup Service',
+    'description': 'Enjoy free pickup service for your laundry orders.',
+  },
+  {
+    'title': 'VIP Washing Package',
+    'description': 'Upgrade to our VIP washing package for premium service.',
+  },
+  {
+    'title': 'Ironing Voucher',
+    'description': 'Receive a voucher for complimentary ironing service.',
+  },
+  {
+    'title': 'Folding Bonus Points',
+    'description': 'Earn bonus points for every item folded with us.',
+  },
+  {
+    'title': 'Free Delivery on Orders',
+    'description': 'Get free delivery for all your laundry orders.',
+  },
+];
